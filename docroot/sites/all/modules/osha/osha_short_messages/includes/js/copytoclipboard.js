e417c3ad0621bdd9c807bdb2ceccf30905880d65
Radu Ciofringeanu <radu.sc@gmail.com>
2015-09-02 19:29:14 +0300

    CW-1739 Short messages html

--- docroot/sites/all/modules/osha/osha_short_messages/includes/js/copytoclipboard.js
+++ docroot/sites/all/modules/osha/osha_short_messages/includes/js/copytoclipboard.js
@@ -10,6 +10,12 @@ jQuery(document).ready(function() {
 
         client.on( 'datarequested', function(client) {
             var iframeContent = jQuery('#edit-short-messages-content-value_ifr').contents();
+
+            //convert relative to absolute url
+            iframeContent.find('a').each(function(){
+                jQuery(this).attr('href', jQuery(this).attr('data-mce-href'));
+            });
+
             //add css styles
             iframeContent.find('.views-field-field-press-contact-job-title').find('div').css({'font-size': '0.9em', 'font-weight': 'bold', 'margin-bottom':'5px'});
             iframeContent.find('.views-field-title').find('a').css({'color': '#003399', 'font-size': '0.9em', 'text-decoration': 'none'});
@@ -17,6 +23,13 @@ jQuery(document).ready(function() {
             iframeContent.find('.views-field-field-press-contact-email').find('a').css({'color': '#003399', 'font-size': '0.9em', 'text-decoration': 'none', 'font-weight': 'bold'});
             iframeContent.find('.view-footer').css({'margin-top':'4px'});
             iframeContent.find('.view-footer').find('a').css({'color': '#003399', 'text-decoration': 'none', 'font-weight': 'bold', 'margin-left':'4px'});
+            iframeContent.find('.node').css({'font-size': '14px'});
+            iframeContent.find('.node').find('a').css({'color': '#039', 'text-decoration': 'none', 'border-bottom': '1px solid #DC2F82'});
+            iframeContent.find('div').css({'margin-top': '0.5em', 'margin-bottom': '0.5em'});
+            iframeContent.find('.node').find('h2').find('a').css({'color': '#039', 'text-decoration': 'none', 'font-weight': 'bold', 'font-size': '1.1em', 'line-height': '1.1em', 'border':0});
+            iframeContent.find('.field-name-field-pr-notes-to-editor').find('.field-label').css({'font-weight': 'bold', 'margin-top': '1.5em'});
+            iframeContent.find('p').css({'line-height': '1.5em', 'margin': '0em'});
+            iframeContent.find('.node-note-to-editor h2').css({'margin': '0px'});
             //get iframe body
             var editorContent = iframeContent.find("body").html();
             client.setText(editorContent);

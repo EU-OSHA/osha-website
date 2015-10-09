f87e65330ddcda8a1e126455c30898b3d19cf033
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-10-09 14:36:24 +0300

    CW-1896 Tag all links inside the newsletter

--- docroot/sites/all/themes/osha_frontend/templates/node--highlights_item.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/node--highlights_item.tpl.php
@@ -62,7 +62,24 @@
                 <?php if (isset($body) && is_array($body)) {
                   if (!empty($body)) {
                     if (isset($body[0]['safe_value'])) {
-                      print($body[0]['safe_value']);
+                      $body_text = $body[0]['safe_value'];
+                      if (isset($variables['elements']['#campaign_id'])) {
+                        // CW-1896 Add pk_campaign to links inside the body text
+                        $doc = new DOMDocument();
+                        $doc->loadHTML(mb_convert_encoding($body_text, 'HTML-ENTITIES', "UTF-8"));
+                        $links = $doc->getElementsByTagName('a');
+                        foreach ($links as $link) {
+                          $url = $link->getAttribute('href');
+                          $link->setAttribute('href', $url . 
+                              (parse_url($url, PHP_URL_QUERY) ? '&' : '?') . 
+                              'pk_campaign=' . $variables['elements']['#campaign_id']
+                          );
+                        }
+                        if ($links->length>0) {
+                          $body_text = $doc->saveHTML();
+                        }
+                      }
+                      print($body_text);
                     }
                   } else if (isset($field_summary) && is_array($field_summary)) {
                     if (!empty($field_summary)) {

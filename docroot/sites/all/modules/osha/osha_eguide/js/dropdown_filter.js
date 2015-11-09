0c1a5b007017cd77e2f8e60023830e2f0ea9fc28
Octavian Belu <belu.octavian@yahoo.com>
2015-11-09 15:45:33 +0200

    Refactor js code

--- docroot/sites/all/modules/osha/osha_eguide/js/dropdown_filter.js
+++ docroot/sites/all/modules/osha/osha_eguide/js/dropdown_filter.js
@@ -3,24 +3,7 @@ jQuery(document).ready(function() {
 });
 
 jQuery(document).ajaxComplete(function(e, xhr, settings) {
-    country_langs = Drupal.settings.osha_eguide.country_langs;
-    country = jQuery('#edit-field-country-tid').val();
-    current_country_langs = country_langs[country];
-    language_dropdown = jQuery('#edit-field-language-value');
-    language_dropdown_current_value = language_dropdown.val();
-    language_dropdown.find('option').remove();
-    for ( i = 0 ; i < current_country_langs.length; i++) {
-        is_selected = language_dropdown_current_value == current_country_langs[i].code ? true : false;
-        option = jQuery('<option>', {
-            value: current_country_langs[i].code,
-            text: current_country_langs[i].language
-        });
-        if (is_selected) {
-            option.attr({'selected': 'selected'});
-        }
-        language_dropdown.append( option );
-    }
-
+    filter_language_list();
     jQuery('#edit-field-country-tid').change(filter_language_list);
 
 });

43e3e07cdce9dba0c1199d981297b0dc10ae82ea
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-11-12 19:03:54 +0200

    MC-19 Removed duplicate id of oshwiki featured articles

--- docroot/sites/all/modules/osha/osha_blocks/templates/oshwiki_featured_articles.tpl.php
+++ docroot/sites/all/modules/osha/osha_blocks/templates/oshwiki_featured_articles.tpl.php
@@ -1,5 +1,5 @@
 <?php if (!empty($tagged_wikis)): ?>
-<div id="related-wiki" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
 <?php
   foreach ($tagged_wikis as $wiki) {
     print render($wiki);

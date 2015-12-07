fa1dbc4af141e5d8e6b6a92ffa5b2b34d4fd9bbd
Octavian Belu <belu.octavian@yahoo.com>
2015-12-07 11:58:49 +0200

    MC-84 Hide country and city from backend when selecting webminar type

---
+++ docroot/sites/all/modules/osha/osha_events/js/event_form.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+    var type_input = jQuery('#edit-field-event-type-und');
+    var country_div = jQuery('#edit-field-country-code');
+    var city_div = jQuery('#edit-field-city');
+    type_input.change(function() {
+        if (jQuery(this).val() == 'Webminar') {
+            country_div.hide();
+            city_div.hide();
+        }
+        else {
+            country_div.show();
+            city_div.show();
+        }
+    });
+});

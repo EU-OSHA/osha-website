e69cc96784330b2c6a6529f65fd9064162667d93
Octavian Belu <belu.octavian@yahoo.com>
2015-12-07 12:22:48 +0200

    MC-84 Hide country and city when an webminar event form loads

--- docroot/sites/all/modules/osha/osha_events/js/event_form.js
+++ docroot/sites/all/modules/osha/osha_events/js/event_form.js
@@ -2,6 +2,10 @@ jQuery(document).ready(function() {
     var type_input = jQuery('#edit-field-event-type-und');
     var country_div = jQuery('#edit-field-country-code');
     var city_div = jQuery('#edit-field-city');
+    if (type_input.val() == 'Webminar') {
+        country_div.hide();
+        city_div.hide();
+    }
     type_input.change(function() {
         if (jQuery(this).val() == 'Webminar') {
             country_div.hide();

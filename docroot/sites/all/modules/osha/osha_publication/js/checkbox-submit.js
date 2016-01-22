280a7b9d041142467a157ce5a4cbb990c0511c7c
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2016-01-22 11:55:08 +0200

    Publication search - auto submit on select change

--- docroot/sites/all/modules/osha/osha_publication/js/checkbox-submit.js
+++
@@ -1,9 +0,0 @@
-(function($){
-    Drupal.behaviors.osha_publication = {
-        attach: function(context, settings) {
-            $('#osha-publication-menu-publications-form input[type=checkbox]', context).click(function(){
-                $('#osha-publication-menu-publications-form').submit();
-            });
-        }
-    }
-})(jQuery);

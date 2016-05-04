1b0ad477ebf993bbb1047213b917110b884ba26d
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-05-04 12:38:45 +0300

    MC-255 - event list start date - end date default

---
+++ docroot/sites/all/modules/osha/osha_events/js/event_search_page.js
@@ -0,0 +1,10 @@
+(function ($) {
+    $(document).ready(function(){
+        $('[name="field_start_date_value[value][date]"]').on('change', function(){
+            var $end_date = $('[name="field_start_date_value2[value][date]"]');
+            if ($end_date.val() == '') {
+                $end_date.datepicker('option', {defaultDate: $(this).val()});
+            }
+        });
+    });
+})(jQuery);

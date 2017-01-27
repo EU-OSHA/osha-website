4605aad63352f0113c49e62d3a96c935ff948ecc
Claudia Ifrim <ifrim.claudia@gmail.com>
2017-01-27 14:28:29 +0200

    MDR-843 - add print friendly into header

--- docroot/sites/all/themes/osha_frontend/templates/region--header.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/region--header.tpl.php
@@ -9,7 +9,7 @@
 	</div>
 </div>
 <div id="languagesAndSearch">
-	<div id="contact"><?php print l(t('News & Events'), 'news-events', array('attributes' => array('accesskey' => '2'))) ?> | <?php print l(t('Press'), 'about-eu-osha/press-room') ?> |  <?php print l(t('Contact us'), 'contact-us') ?> <span class="a_small"><a  onclick="zoomSmall()">a</a></span><span class="a_medium"><a onclick="zoomMedium()">a</a></span><span class="a_big"><a  onclick="zoomBig()">a</a></span></div>
+	<div id="contact"><?php print l(t('News & Events'), 'news-events', array('attributes' => array('accesskey' => '2'))) ?> | <?php print l(t('Press'), 'about-eu-osha/press-room') ?> |  <?php print l(t('Contact us'), 'contact-us') ?> <span class="a_small"><a  onclick="zoomSmall()">a</a></span><span class="a_medium"><a onclick="zoomMedium()">a</a></span><span class="a_big"><a  onclick="zoomBig()">a</a></span> |  <a href="#" onclick="if(window.print)window.print();" class="printfriendly" title="Print page"><span class="glyphicon glyphicon-print"></span></a></div>
 	<div>
 		<img src="/sites/all/themes/osha_frontend/images/languageico.png" alt="Select language">
 	</div>

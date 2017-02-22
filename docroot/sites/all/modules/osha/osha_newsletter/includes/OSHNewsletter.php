0af7451ce93fabc586869f6b9011b595c937834e
Octavian Belu <beluoctavian@gmail.com>
2017-02-22 15:23:39 +0200

    Refactor newsletter rendering

--- docroot/sites/all/modules/osha/osha_newsletter/includes/OSHNewsletter.php
+++ docroot/sites/all/modules/osha/osha_newsletter/includes/OSHNewsletter.php
@@ -37,20 +37,15 @@ class OSHNewsletter {
   }
 
   public static function render(EntityCollection $source) {
-    $content = entity_collection_load_content($source->bundle, $source);
+    $content = [];
+    $entityCollectionItems = entity_collection_load_content($source->bundle, $source);
 
-    $newsletter_title = $source->title;
-    $newsletter_id = $source->eid;
-    $newsletter_date = $source->field_created[LANGUAGE_NONE][0]['value'];
-    if (!empty($source->field_publication_date)) {
-      $newsletter_date = $source->field_publication_date[LANGUAGE_NONE][0]['value'];
-    }
     $campaign_id = '';
-    if (isset($source->field_campaign_id[LANGUAGE_NONE][0]['value'])) {
+    if (!empty($source->field_campaign_id[LANGUAGE_NONE][0]['value'])) {
       $campaign_id = $source->field_campaign_id[LANGUAGE_NONE][0]['value'];
     };
 
-    $items = $content->children;
+    $items = $entityCollectionItems->children;
     $elements = array();
     $last_section = NULL;
     $blogs = array();
@@ -60,6 +55,7 @@ class OSHNewsletter {
     // @todo: replace hardcoded sections with taxonomy terms
     foreach ($items as $item) {
       if ($item->type == 'taxonomy_term') {
+        // Section
         $term = taxonomy_term_view($item->content, 'token');
         $last_section = $item->content->name_original;
         if ($last_section == 'Blog') {
@@ -101,9 +97,12 @@ class OSHNewsletter {
     return [
       'header' => theme('newsletter_header', array(
         'languages' => $languages,
-        'newsletter_title' => $newsletter_title,
-        'newsletter_id' => $newsletter_id,
-        'newsletter_date' => $newsletter_date,
+        'newsletter_title' => $source->title,
+        'newsletter_id' => $source->eid,
+        'newsletter_date' =>
+          !empty($source->field_publication_date)
+          ? $source->field_publication_date[LANGUAGE_NONE][0]['value']
+          : $source->field_created[LANGUAGE_NONE][0]['value'],
         'campaign_id' => $campaign_id
       )),
       'body' =>  osha_newsletter_format_body($body), //add css styles to href in body

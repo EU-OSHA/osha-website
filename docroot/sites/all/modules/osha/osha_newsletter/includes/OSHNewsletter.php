91376d86657c3adbd7483ff46cb6aca133c35fd0
Octavian Belu <beluoctavian@gmail.com>
2017-02-23 10:35:38 +0200

    Render sections as tables

--- docroot/sites/all/modules/osha/osha_newsletter/includes/OSHNewsletter.php
+++ docroot/sites/all/modules/osha/osha_newsletter/includes/OSHNewsletter.php
@@ -14,6 +14,21 @@ class OSHNewsletter {
     ];
   }
 
+  public static function getChildStyle($parentTemaplate, $default = 'newsletter_item') {
+    $styles = [
+      'newsletter_full_width_details' => 'highlights_item',
+      'newsletter_full_width_list' => 'newsletter_item',
+      'newsletter_half_width_details' => 'highlights_item',
+      'newsletter_half_width_list' => 'newsletter_item',
+      'newsletter_half_image_left' => 'newsletter_item',
+      'newsletter_full_width_2_col_blocks' => 'newsletter_item'
+    ];
+    if (empty($styles[$parentTemaplate])) {
+      return $default;
+    }
+    return $styles[$parentTemaplate];
+  }
+
   public static function alterContentForm(&$form, &$form_state) {
     // add submit button to send newsletter and send test newsletter
     if (isset($form['content'])) {
@@ -45,6 +60,24 @@ class OSHNewsletter {
     }
   }
 
+  public static function renderTemplate($template, $variables) {
+    switch ($template) {
+      case 'newsletter_full_width_details';
+        $content = [
+          'header' => ['data' => taxonomy_term_view($variables['section'], 'token')],
+          'rows' => [],
+        ];
+        foreach ($variables['nodes'] as $node) {
+          $content['rows'][] = [
+            'data' => [node_view($node['node'], $node['#style'])],
+          ];
+        }
+        return render($content);
+      default:
+        return theme($template, $variables);
+    }
+  }
+
   public static function render(EntityCollection $source) {
     $content = [];
     $entityCollectionItems = entity_collection_load_content($source->bundle, $source);
@@ -62,12 +95,13 @@ class OSHNewsletter {
     $events = array();
 
     // @todo: replace hardcoded sections with taxonomy terms
+    $templatesList = self::getTemplatesList();
     foreach ($items as $item) {
       switch ($item->type) {
         case 'taxonomy_term':
           $current_section = $item->entity_id;
           $content[$current_section] = [
-            '#style' => $item->style,
+            '#style' => !empty($item->style) ? $item->style : key($templatesList),
             'section' => $item->content,
             'nodes' => [],
           ];
@@ -79,51 +113,56 @@ class OSHNewsletter {
             continue;
           }
           $content[$current_section]['nodes'][] = [
-            '#style' => $item->style,
+            '#style' => self::getChildStyle($content[$current_section]['#style']),
             'node' => $item->content,
           ];
           break;
       }
 
-      if ($item->type == 'taxonomy_term') {
-        // Section
-        $term = taxonomy_term_view($item->content, 'token');
-        $last_section = $item->content->name_original;
-        if ($last_section == 'Blog') {
-          $blogs[] = $term;
-        } else if ($last_section == 'News') {
-          $news[] = $term;
-        } else if ($last_section == 'Events') {
-          $events[] = $term;
-        } else {
-          $elements[] = $term;
-        }
-      } elseif ($item->type == 'node') {
-        $style = $item->style;
-        $node = node_view($item->content,$style);
-        $node['#campaign_id'] = $campaign_id;
-
-        if ($last_section == 'Blog') {
-          $blogs[] = $node;
-        } else if ($last_section == 'News') {
-          $news[] = $node;
-        } else if ($last_section == 'Events') {
-          $events[] = $node;
-        } else {
-          $elements[] = $node;
-        }
-      }
+//      if ($item->type == 'taxonomy_term') {
+//        // Section
+//        $term = taxonomy_term_view($item->content, 'token');
+//        $last_section = $item->content->name_original;
+//        if ($last_section == 'Blog') {
+//          $blogs[] = $term;
+//        } else if ($last_section == 'News') {
+//          $news[] = $term;
+//        } else if ($last_section == 'Events') {
+//          $events[] = $term;
+//        } else {
+//          $elements[] = $term;
+//        }
+//      } elseif ($item->type == 'node') {
+//        $style = $item->style;
+//        $node = node_view($item->content,$style);
+//        $node['#campaign_id'] = $campaign_id;
+//
+//        if ($last_section == 'Blog') {
+//          $blogs[] = $node;
+//        } else if ($last_section == 'News') {
+//          $news[] = $node;
+//        } else if ($last_section == 'Events') {
+//          $events[] = $node;
+//        } else {
+//          $elements[] = $node;
+//        }
+//      }
     }
 
     $languages = osha_language_list(TRUE);
 
-    $body = theme('newsletter_body', array(
-      'items' => $elements,
-      'blogs' => $blogs,
-      'news' => $news,
-      'events' => $events,
-      'campaign_id' => $campaign_id
-    ));
+    $renderedContent = '';
+    foreach ($content as $section) {
+      $renderedContent .= self::renderTemplate($section['#style'], $section);
+    }
+
+//    $body = theme('newsletter_body', array(
+//      'items' => $elements,
+//      'blogs' => $blogs,
+//      'news' => $news,
+//      'events' => $events,
+//      'campaign_id' => $campaign_id
+//    ));
 
     return [
       'header' => theme('newsletter_header', array(
@@ -136,7 +175,7 @@ class OSHNewsletter {
           : $source->field_created[LANGUAGE_NONE][0]['value'],
         'campaign_id' => $campaign_id
       )),
-      'body' =>  osha_newsletter_format_body($body), //add css styles to href in body
+      'body' =>  $renderedContent, //add css styles to href in body
       'footer' => theme('newsletter_footer', array('campaign_id' => $campaign_id)),
     ];
   }

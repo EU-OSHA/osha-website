d0e094407c581189dfd0a5ca139413defc8e3ec6
dragos-dumi <dumitrescu.dragos1@gmail.com>
2017-03-08 11:13:03 +0200

    Removed a leftover dpm

--- docroot/sites/all/themes/osha_frontend/templates/field--field-aditional-resources.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/field--field-aditional-resources.tpl.php
@@ -31,7 +31,7 @@
       ?>
       <?php if (in_array($node_type, ['youtube', 'flickr', 'slideshare'])) { ?>
         <?php // Render without field list for each node, all items to be on same level. ?>
-        <?php dpm($node_type); print render($item['node'][current(element_children($item['node']))]['field_' . $node_type]); ?>
+        <?php print render($item['node'][current(element_children($item['node']))]['field_' . $node_type]); ?>
       <?php } else { ?>
         <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
           <?php print render($item); ?>

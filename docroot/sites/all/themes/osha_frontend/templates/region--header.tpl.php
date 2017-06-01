13ad8142720b89f1f7fe4bc2cf81c45421e60c5b
Endika Llonin <ellonin@bilbomatica.es>
2017-06-01 11:37:51 +0200

    MDR-1170 v4

--- docroot/sites/all/themes/osha_frontend/templates/region--header.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/region--header.tpl.php
@@ -15,7 +15,7 @@
 	</div>
 </div>
 <?php
-$url = url(drupal_get_path_alias('node/11000',$language->language;));
+$url = url(drupal_get_path_alias('node/11000',$language->language));
 ?>
 
 <div id="agencyLogo"><a href="<?php echo url('<front>');?>"><img src="/sites/all/themes/osha_frontend/images/eu-osha-logo/EU-OSHA-<?php global $language;print (in_array($language->language, osha_enabled_language_list()))?$language->language:'en';?>.png" alt="<?php echo t('European Agency for Safety and Health at Work');?>"></a></div>

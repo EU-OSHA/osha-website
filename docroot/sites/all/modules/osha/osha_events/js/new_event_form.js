6241b7c83511ee7b86d6d40fbc7254d52c47cbb4
Claudia Ifrim <ifrim.claudia@gmail.com>
2017-06-13 23:34:18 +0300

    MDR-1142 - mandatory fields + validation

--- docroot/sites/all/modules/osha/osha_events/js/new_event_form.js
+++ docroot/sites/all/modules/osha/osha_events/js/new_event_form.js
@@ -1,4 +1,11 @@
 jQuery(document).ready(function() {
+    var tags_div = jQuery('#edit-field-tags');
+    var organization_div = jQuery('#edit-field-organization');
+    jQuery('.description', tags_div).text('');
+    jQuery('.description', organization_div).text('');
+    jQuery('.form-item-field-tags-und > label').find('span').remove();
+    jQuery('.form-item-field-organization-und-0-value > label').find('span').remove();
+
     var start_date = jQuery('#edit-field-start-date-und-0-value-datepicker-popup-0');
     var end_date = jQuery('#edit-field-start-date-und-0-value2-datepicker-popup-0');
     start_date.change(function() {

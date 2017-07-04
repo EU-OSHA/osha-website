ebc2873b1261fdab0fb2f6961fc2e75b7c14d69e
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2017-07-04 10:32:59 +0300

    MDR-1306 DS database - option to export results to rtf is missing

---
+++ docroot/sites/all/modules/osha/osha_dangerous_substances/templates/osha_dangerous_substances_doc_header.tpl.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <style type="text/css">
+
+          .tableBorder
+          {
+              background-color: #fff;
+              width: 100%;
+              border-collapse: collapse;
+          }
+
+          .tableBorder td
+          {
+              vertical-align: top;
+              padding: 2px;
+              border: 1px solid #ccc;
+          }
+
+          hr
+          {
+              border-top:1px solid #aaa;
+          }
+      </style>
+  </head>
+  <body>

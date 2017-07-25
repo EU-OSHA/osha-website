44d368f31219b03f501deabdee6e99db29c1c0f0
Claudia Ifrim <ifrim.claudia@gmail.com>
2017-07-25 15:41:45 +0300

    MDR-1349 - make field category/tag required for events content type

--- docroot/sites/all/modules/osha/osha_events/js/new_event_form.js
+++ docroot/sites/all/modules/osha/osha_events/js/new_event_form.js
@@ -1,9 +1,7 @@
 jQuery(document).ready(function() {
-    var tags_div = jQuery('#edit-field-tags');
     var organization_div = jQuery('#edit-field-organization');
-    jQuery('.description', tags_div).text('');
+
     jQuery('.description', organization_div).text('');
-    jQuery('.form-item-field-tags-und > label').find('span').remove();
     jQuery('.form-item-field-organization-und-0-value > label').find('span').remove();
 
     var start_date = jQuery('#edit-field-start-date-und-0-value-datepicker-popup-0');
--- docroot/sites/all/modules/osha/osha_events/js/published_event_form.js
+++ docroot/sites/all/modules/osha/osha_events/js/published_event_form.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function() {
-    var tags_div = jQuery('#edit-field-tags');
     var organization_div = jQuery('#edit-field-organization');
-    jQuery('.description', tags_div).text(Drupal.t('Mandatory'));
+
     jQuery('.description', organization_div).text(Drupal.t('Mandatory'));
-    jQuery('.form-item-field-tags-und > label').append('<span class="form-required" title="This field is required.">*</span>');
     jQuery('.form-item-field-organization-und-0-value > label').append('<span class="form-required" title="This field is required.">*</span>');
 });

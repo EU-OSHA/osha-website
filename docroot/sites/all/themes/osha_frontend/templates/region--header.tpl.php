8ebde80bea93ed18124e3d4fa41cc29ba33be834
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2018-05-28 15:26:11 +0300

    MDR-1948 add names to the icons for social media (corporate)

--- docroot/sites/all/themes/osha_frontend/templates/region--header.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/region--header.tpl.php
@@ -1,11 +1,11 @@
 <div id="socialNetworksBlue">
 	<div id="icons">
-		<a target="_blank" href="https://twitter.com/eu_osha"><img src="/sites/all/themes/osha_frontend/images/twitter-icon.png" alt="EU OSHA in Twitter"></a>
-		<a target="_blank" href="https://www.facebook.com/EuropeanAgencyforSafetyandHealthatWork"><img src="/sites/all/themes/osha_frontend/images/facebook-icon.png" alt="EU OSHA Facebook Page"></a>
-		<a target="_blank" href="http://www.linkedin.com/company/european-agency-for-safety-and-health-at-work"><img src="/sites/all/themes/osha_frontend/images/linkedin-icon.png" alt="Follow EU OSHA in LinkedIn"></a>
-		<a target="_blank" href="https://www.youtube.com/user/EUOSHA"><img src="/sites/all/themes/osha_frontend/images/youtube-icon.png" alt="EU OSHA channel in Youtube"></a>
-		<a target="_blank" href="https://www.flickr.com/photos/euosha/albums"><img src="/sites/all/themes/osha_frontend/images/flickr-icon.png" alt="EU OSHA Flickr photos"></a>
-		<a target="_blank" href="<?php echo url('tools-and-publications/blog', array('alias' => TRUE)); ?>"><img src="/sites/all/themes/osha_frontend/images/blogger-icon.png" alt="EU OSHA Blog with more information in workplace safety and health"></a>
+		<a target="_blank" href="https://twitter.com/eu_osha"><img src="/sites/all/themes/osha_frontend/images/twitter-icon.png" alt="EU OSHA in Twitter" title="Twitter"></a>
+		<a target="_blank" href="https://www.facebook.com/EuropeanAgencyforSafetyandHealthatWork"><img src="/sites/all/themes/osha_frontend/images/facebook-icon.png" alt="EU OSHA Facebook Page" title="Facebook"></a>
+		<a target="_blank" href="http://www.linkedin.com/company/european-agency-for-safety-and-health-at-work"><img src="/sites/all/themes/osha_frontend/images/linkedin-icon.png" alt="Follow EU OSHA in LinkedIn" title="LinkedIn"></a>
+		<a target="_blank" href="https://www.youtube.com/user/EUOSHA"><img src="/sites/all/themes/osha_frontend/images/youtube-icon.png" alt="EU OSHA channel in Youtube" title="Youtube"></a>
+		<a target="_blank" href="https://www.flickr.com/photos/euosha/albums"><img src="/sites/all/themes/osha_frontend/images/flickr-icon.png" alt="EU OSHA Flickr photos" title="Flickr"></a>
+		<a target="_blank" href="<?php echo url('tools-and-publications/blog', array('alias' => TRUE)); ?>"><img src="/sites/all/themes/osha_frontend/images/blogger-icon.png" alt="EU OSHA Blog with more information in workplace safety and health" title="Blog"></a>
 	</div>
 </div>
 <div id="languagesAndSearch">

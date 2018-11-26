ecbe7c12317fba2f60be4dd5b0382ee5cab837b7
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2018-11-26 18:49:09 +0200

    MDR-2335 Could broken links in a page be highlighted when in view mode

---
+++ docroot/sites/all/libraries/tinymce/jscripts/tiny_mce/plugins/linkchecker/editor_plugin.js
@@ -0,0 +1,40 @@
+/**
+ * editor_plugin.js
+ */
+
+(function() {
+    tinymce.create('tinymce.plugins.Linkchecker', {
+        id : null,
+
+        init : function (ed, url) {
+            var t = this;
+            ed.onInit.add(function (ed) {
+                t._update(ed);
+            });
+        },
+
+        _update : function (ed) {
+            var t = this;
+            if (Drupal.settings.osha_linkchecker.urls) {
+                var tx = ed.getContent();
+                for (var i = 0; i < Drupal.settings.osha_linkchecker.urls.length; i++) {
+                    var url = Drupal.settings.osha_linkchecker.urls[i];
+                    if (tx.indexOf('href="' + url) > 0) {
+                        tx = tx.replace('href="' + url, 'data-linkchecker="" href="' + url);
+                    }
+                }
+                ed.setContent(tx);
+            }
+
+        },
+
+        getInfo: function () {
+            return {
+                longname : 'Linkchecker',
+                version : tinymce.majorVersion + "." + tinymce.minorVersion
+            };
+        }
+    });
+
+    tinymce.PluginManager.add('linkchecker', tinymce.plugins.Linkchecker);
+})();

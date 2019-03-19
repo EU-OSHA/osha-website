c40282c5d334ec47d9294de0f1cd1b45787bd76c
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-03-19 11:39:37 +0200

    MDR-2609 NCW Backend Events List - several improvements

--- docroot/sites/all/modules/osha/osha_events/js/new_event_form.js
+++ docroot/sites/all/modules/osha/osha_events/js/new_event_form.js
@@ -1,7 +1,4 @@
 jQuery(document).ready(function() {
-    var organization_div = jQuery('#edit-field-organization');
-
-    jQuery('.description', organization_div).text('');
     jQuery('.form-item-field-organization-und-0-value > label').find('span').remove();
 
     var start_date = jQuery('#edit-field-start-date-und-0-value-datepicker-popup-0');

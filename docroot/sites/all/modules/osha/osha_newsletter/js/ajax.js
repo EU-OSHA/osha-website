a5ecf79e037ca70336f9130efdaa325c138b1932
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-05-06 17:31:08 +0300

    MDR-2771 OSHMail subscription validation

--- docroot/sites/all/modules/osha/osha_newsletter/js/ajax.js
+++ docroot/sites/all/modules/osha/osha_newsletter/js/ajax.js
@@ -1,17 +1,29 @@
 (function($){
   Drupal.behaviors.osha_newsletter_captcha_block = {
     attach: function (context, settings) {
-      $('#edit-email').once('osha_newsletter_captcha_block', function(){
+      $('#edit-email').once('osha_newsletter_captcha_block', function() {
         var ajax_settings = {};
         ajax_settings.url = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'newsletter/ajax/block';
         ajax_settings.event = 'click';
         var base = '#edit-email';
         Drupal.ajax['captcha-block'] = new Drupal.ajax(base, this, ajax_settings);
       });
+      if (eval('typeof URLSearchParams')) {
+        var searchParams = new URLSearchParams(window.location.search);
+        var agree = searchParams.get('agree');
+        var email = searchParams.get('email');
+        if (email) {
+          jQuery('div.form-item-email input.form-text').val(Drupal.checkPlain(email));
+        }
+        if (agree) {
+          jQuery(".form-item-agree-processing-personal-data input.form-checkbox").prop('checked', true);
+        }
+      }
     }
   };
 })(jQuery);
-(function($){
+
+(function($) {
   $.fn.captcha_init = function() {
     captcha.init();
   };

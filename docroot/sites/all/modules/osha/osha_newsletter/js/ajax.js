f95aa384c31e78258c1db674997424d25cf9e375
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-05-07 11:22:27 +0300

    MDR-2786 NCW - Revamp - Home page development

--- docroot/sites/all/modules/osha/osha_newsletter/js/ajax.js
+++ docroot/sites/all/modules/osha/osha_newsletter/js/ajax.js
@@ -1,17 +1,24 @@
 (function($){
   Drupal.behaviors.osha_newsletter_captcha_block = {
     attach: function (context, settings) {
-      $('#edit-email').once('osha_newsletter_captcha_block', function(){
+      $('#edit-email').once('osha_newsletter_captcha_block', function() {
         var ajax_settings = {};
         ajax_settings.url = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'newsletter/ajax/block';
         ajax_settings.event = 'click';
         var base = '#edit-email';
         Drupal.ajax['captcha-block'] = new Drupal.ajax(base, this, ajax_settings);
       });
+      $('#edit-email--2').once('osha_newsletter_captcha_block', function() {
+        var ajax_settings = {};
+        ajax_settings.url = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'newsletter/ajax/block/footer';
+        ajax_settings.event = 'click';
+        var base = '#edit-email--2';
+        Drupal.ajax['captcha-block-2'] = new Drupal.ajax(base, this, ajax_settings);
+      });
     }
   };
 })(jQuery);
-(function($){
+(function($) {
   $.fn.captcha_init = function() {
     captcha.init();
   };

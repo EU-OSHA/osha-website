5283581f0a828a0ff757a8658e60494c446b4b2e
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-05-10 12:49:35 +0300

    MDR-2883 Footer - Sitemap (display menu level 1 and 3)

--- docroot/sites/all/modules/osha/osha_homepage/templates/footer-menu-block.tpl.php
+++ docroot/sites/all/modules/osha/osha_homepage/templates/footer-menu-block.tpl.php
@@ -1,6 +1,10 @@
+<?php
+$skip = variable_get('footer_menu_skip_empty', FALSE);
+$exclude_url = explode("\n", variable_get('footer_menu_exclude_url', ''));
+?>
 <div class="footer footer-line footer-site-map container"><ul>
 <?php foreach ($menu_data as $menu) {
-  if (!$menu['below']) {
+  if ((!$menu['below'] && $skip) || (in_array($menu['link']['href'], $exclude_url))) {
     continue;
   }
   ?>
@@ -8,7 +12,11 @@
     <ul>
       <li><?php  print l($menu['link']['title'], $menu['link']['href']); ?></li>
       <li><ul>
-<?php foreach ($menu['below'] as $submenu) { ?>
+<?php foreach ($menu['below'] as $submenu) {
+    if (in_array($submenu['link']['href'], $exclude_url)) {
+        continue;
+    }
+    ?>
       <li><?php print l($submenu['link']['title'], $submenu['link']['href']); ?></li>
 <?php } ?>
         </ul>

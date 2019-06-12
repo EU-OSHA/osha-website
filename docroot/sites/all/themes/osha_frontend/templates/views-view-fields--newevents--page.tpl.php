2fa904928eefedc49600339f44b5fa879009e48d
mcacheiro <[mcacheiro@bilbomatica.es]>
2019-06-12 14:31:46 +0200

    MDR-2843 - NCW - Revamp - Events list - Development

--- docroot/sites/all/themes/osha_frontend/templates/views-view-fields--newevents--page.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/views-view-fields--newevents--page.tpl.php
@@ -62,7 +62,7 @@ if ($node->field_show_eu_flag && $node->field_show_eu_flag['und'][0]['value']) {
     ));
     ?>
     <?php if ($end_date) { ?>
-      <a class="add-to-my-calendar" id="add-to-calendar-<?php print $nid; ?>" href="#"></a>
+      <span class="add-to-my-calendar" id="add-to-calendar-<?php print $nid; ?>" href="#"></span>
       <script>
           var myCalendar<?php print $nid; ?> = createCalendar({
               options: {

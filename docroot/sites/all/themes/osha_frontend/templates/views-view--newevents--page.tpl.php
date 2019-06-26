ae1fdb96e9d664768475ea11e97b4a10e1ca04fe
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-06-26 06:37:41 +0300

    MDR-2993 OUR STORY; View more does not work

--- docroot/sites/all/themes/osha_frontend/templates/views-view--newevents--page.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/views-view--newevents--page.tpl.php
@@ -34,61 +34,59 @@
   <?php endif; ?>
   <?php print render($title_suffix); ?>
   <?php if ($header): ?>
-    <div class="view-header">
-      <?php print $header; ?>
-    </div>
+      <div class="view-header">
+        <?php print $header; ?>
+      </div>
   <?php endif; ?>
 
   <?php if ($exposed): ?>
-    <div class="view-filters">
-      <?php print $exposed; ?>
-    </div>
+      <div class="view-filters">
+        <?php print $exposed; ?>
+      </div>
   <?php endif; ?>
 
   <?php if ($attachment_before): ?>
-    <div class="attachment attachment-before">
-      <?php print $attachment_before; ?>
-    </div>
+      <div class="attachment attachment-before">
+        <?php print $attachment_before; ?>
+      </div>
   <?php endif; ?>
 
   <?php if ($rows): ?>
-    <div class="view-content">
-      <?php
-      $rows = str_replace('<h3>', '<div class="events-view"><h2><span>', $rows);
-      $rows = str_replace('</h3>', '</span></h2></div>', $rows);
-      print $rows;
-      ?>
-    </div>
+      <div class="view-content">
+        <?php
+        $rows = str_replace('<h3>', '<div class="events-view"><h2><span>', $rows);
+        $rows = str_replace('</h3>', '</span></h2></div>', $rows);
+        print $rows;
+        ?>
+      </div>
   <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
-    </div>
+      <div class="view-empty">
+        <?php print $empty; ?>
+      </div>
   <?php endif; ?>
-
-  <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
-
   <?php if ($attachment_after): ?>
-    <div class="attachment attachment-after">
-      <?php print $attachment_after; ?>
-    </div>
+      <div class="attachment attachment-after">
+        <?php print $attachment_after; ?>
+      </div>
   <?php endif; ?>
 
   <?php if ($more): ?>
     <?php print $more; ?>
   <?php endif; ?>
 
-  <?php if ($footer): ?>
-    <div class="view-footer">
-      <?php print $footer; ?>
-    </div>
-  <?php endif; ?>
 
   <?php if ($feed_icon): ?>
-    <div class="feed-icon">
-      <?php print $feed_icon; ?>
-    </div>
+      <div class="feed-icon">
+        <?php print $feed_icon; ?>
+      </div>
   <?php endif; ?>
 
 </div><?php /* class view */ ?>
+<div class="content-pagination container">
+  <?php if ($pager): ?>
+    <?php print $pager; ?>
+  <?php endif; ?>
+  <?php if ($footer): ?>
+    <?php print $footer; ?>
+  <?php endif; ?>
+</div>

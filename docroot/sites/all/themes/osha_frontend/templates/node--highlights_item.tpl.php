3dfe082c05d931e820270091ebc95434e24ceb04
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-10-09 13:01:51 +0300

    MDR-3317 The LVs of highlights in OSHmail appear in EN, only the titles are translated

--- docroot/sites/all/themes/osha_frontend/templates/node--highlights_item.tpl.php
+++ docroot/sites/all/themes/osha_frontend/templates/node--highlights_item.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * EU-OSHA's theme implementation to display a newsletter item in Newsletter Highlights view mode.
@@ -131,34 +130,8 @@ if (!empty($campaign_id)) {
                     <td colspan="2" style="width: 100%; font-size: 13px; font-family: Arial, sans-serif; color: #000000;">
                       <?php
                   if (!$hide_summary) {
-
-                      $body_text = '';
-                      if (isset($field_summary) && is_array($field_summary) && !empty($field_summary)) {
-                        $body_text = field_view_field('node', $node, 'field_summary', 'highlights_item');
-                      }
-                      elseif (isset($body) && is_array($body) && !empty($body)) {
-                        $body_text = field_view_field('node', $node, 'body', 'highlights_item');
-                      }
-                      $body_text = render($body_text);
-                      if (!empty($body_text)) {
-                        if (!empty($campaign_id)) {
-                          // CW-1896 Add pk_campaign to links inside the body text.
-                          $doc = new DOMDocument();
-                          $doc->loadHTML(mb_convert_encoding($body_text, 'HTML-ENTITIES', "UTF-8"));
-                          $links = $doc->getElementsByTagName('a');
-                          foreach ($links as $link) {
-                            $url = $link->getAttribute('href');
-                            $url_comp = parse_url($url);
-                            if (preg_match('/(osha.europa.eu|napofilm.net|oshwiki.eu|oiraproject.eu|esener.eu|healthy-workplaces.eu|healthyworkplaces.eu|localhost|eu-osha.bilbomatica.es)/', $url_comp['host'])) {
-                              $link->setAttribute('href', $url . ($url_comp['query'] ? '&' : '?') . 'pk_campaign=' . $campaign_id);
-                            }
-                          }
-                          if ($links->length > 0) {
-                            $body_text = $doc->saveHTML();
-                          }
-                        }
-
-                        $body_summary_highlight = render($node->body[$language][0]['safe_value']);
+                      $body_summary_highlight = $body[0]['safe_value'];
+                      if (!empty($body_summary_highlight)) {
                         if (trim(strip_tags($body_summary_highlight))) {
                           $body_summary_highlight = strip_tags($body_summary_highlight);
                           $body_summary_highlight = substr($body_summary_highlight, 0, 260);

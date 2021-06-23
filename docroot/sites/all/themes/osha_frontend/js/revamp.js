a20e98ce0cb28c143612d4420de42aeeb03f6dca
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2021-06-23 16:14:04 +0300

    REL-2341 RELEASE PRD NCW - 23/06/2021

--- docroot/sites/all/themes/osha_frontend/js/revamp.js
+++ docroot/sites/all/themes/osha_frontend/js/revamp.js
@@ -124,34 +124,6 @@ jQuery(document).ready(function($){
 		$( "div.view-news-and-events > div.row > div:nth-child(3) > div:nth-child(2) > h3 a" ).append( "..." );
 	}
 
-	/*Ellipsis Home intro boxes*/
-
-	var character_intro = 72;
-	var count1_intro = $(".home-intro > div:nth-child(1) > a:nth-child(1) > h2").text().length;
-	var count2_intro = $(".home-intro > div:nth-child(2) > a:nth-child(1) > h2").text().length;
-	var count3_intro = $(".home-intro > div:nth-child(3) > a:nth-child(1) > h2").text().length;
-
-	if (count1_intro >= character_intro){
-		$(".home-intro > div:nth-child(1) > a:nth-child(1) > h2").text(function(){
-    		return $(this).text().substring(0,character_intro);
-		});
-		$( ".home-intro > div:nth-child(1) > a:nth-child(1) > h2" ).append( "..." );
-	}
-
-	if (count2_intro >= character_intro){
-		$(".home-intro > div:nth-child(2) > a:nth-child(1) > h2").text(function(){
-    		return $(this).text().substring(0,character_intro);
-		});
-		$( ".home-intro > div:nth-child(2) > a:nth-child(1) > h2" ).append( "..." );
-	}
-
-	if (count3_intro >= character_intro){
-		$(".home-intro > div:nth-child(3) > a:nth-child(1) > h2").text(function(){
-    		return $(this).text().substring(0,character_intro);
-		});
-		$( ".home-intro > div:nth-child(3) > a:nth-child(1) > h2" ).append( "..." );
-	}
-
 	/* Cookies declined */
 	$(".decline-button").click(function() {
 		$('#sliding-popup').remove();
